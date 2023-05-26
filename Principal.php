73ce5993a3ab664a3a32aae47b56e66dbddc92e4
Denidev <denilsoncostadias0101@gmail.com>
2023-05-25 21:49:04 -0300

    versão com erro no php, ao ir de qualquer categoria para produto

---
+++ Principal.php
@@ -0,0 +1,112 @@
+<?php 
+require ("dadosgerais.php");
+$type = $_GET["type"] ?? '';
+
+    if($type=="mansoes"){
+      $data=$geral["Mansões"];
+    }else if($type=="quartos"){
+      $data=$geral["Quartos"];
+
+    }else if($type=="fazendas"){
+      $data=$geral["Fazendas"];
+
+    }else{
+      $data=$geral;
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Área cheap</title>
+  <link rel="stylesheet"  href="style.css">
+</head>
+
+<body>
+  <header>
+      <a href="principal.php"> <img src="ImgCategorias/logo.png" alt="Logo Área Cheap" id="logo"> </a>
+            <form>
+              <input type="search" name="busca" placeholder="Busca">
+            </form>
+          <a href="cadastro.php">Cadastro</a>
+          <a href="login.php">Login</a>
+          <a href="saibamais.php">Saiba mais</a>
+
+  </header>
+    <nav>
+      <ul>
+        <li><a href="Principal.php">Em Alta</a></li>
+        <li><a href="?type=mansoes">Mansão</a></li>
+        <li><a href="?type=quartos">Quarto</a></li>
+        <li><a href="?type=fazendas">Fazenda</a></li>
+      </ul>
+    </nav>
+
+  <main>
+      
+    <?php
+        if(isset($data["Mansões"]) && isset($data["Fazendas"]) && isset($data["Quartos"])  ){
+          
+          foreach($data as $iUm => $aCategoria){
+            foreach($aCategoria as $iDois=> $resposta){
+    ?>
+      
+    <div class="listing-card">
+        <a href="principalDetalhes.php?index=<?=$iDois?>&type=<?php
+          if($iUm == "Fazendas") echo "fazendas";
+          else if($iUm == "Mansões") echo "mansoes";
+          else if($iUm == "Quartos") echo "quartos";
+        ?>">
+      <img src="<?=$resposta["Foto"]?>" alt="<?=$resposta["Localização"]?>"> 
+     
+      <div>
+        <h4><?=$resposta["Localização"]?></h4> 
+        <h2>R$<?=$resposta["ValorAluguel"]?></h2><p>Aluguel</p> 
+        <h3><?=$resposta["Responsável"]?></h3> 
+      </div>
+      <button>Reservar</button>
+    </a>
+		</div>  
+   
+  <?php
+
+         }
+      }       
+    }else{
+        foreach($data as $iUm => $aCategoria){
+          ?>
+          <div class="listing-card">
+             <a href="principalDetalhes.php">
+            <img src="<?=$aCategoria["Foto"]?>" alt="<?=$aCategoria["Localização"]?>"> 
+           
+            <div>
+              <h3><?=$aCategoria["Responsável"]?></h3> 
+              <h4><?=$aCategoria["Localização"]?></h4> 
+              <h2>R$<?=$aCategoria["ValorAluguel"]?></h2><p>Aluguel</p> 
+            </div>
+            <button>Reservar</button>
+          </a>
+          </div>  
+         
+        <?php
+             }
+          } 
+
+        ?>
+
+ </main>
+  <footer>
+    <div>
+    
+   <p id="pfooter"> Contatos:<br>
+    De segunda a sexta:  (77) 98812-3456<br>
+    Sabados e Domingos:  (77) 98854-6783<br>
+    Endereço:<br> IFBA Campus Brumado <br>
+   </p>
+    </div>
+    &copy; <?php echo date("Y"); ?> Meu Site Airbnb. Todos os direitos reservados.<br>
+  </footer>
+</body>
+</html>
---
+++ principalDetalhes.php
@@ -0,0 +1,96 @@
+<?php 
+require ("dadosgerais.php");
+
+if (!isset($_GET['type']) || !isset($_GET['index'])) {
+  header('Location: Principal.php');
+  exit();
+} else if ($_GET['type'] != 'mansoes' && $_GET['type'] != 'fazendas' && $_GET['type'] != 'quartos') {
+  header('Location: Principal.php');
+  exit();
+}
+
+if ($_GET['type'] == 'fazendas') {
+  if (count($geral["Fazendas"]) < intval($_GET['index'])) {
+    header('Location: Principal.php');
+      exit();
+  } else {
+      $data = $geral["Fazendas"][$_GET['index']];
+  }
+} else if ($_GET['type'] == 'quartos') {
+  if (count($geral["Quartos"]) < intval($_GET['index'])) {
+    header('Location: Principal.php');
+      exit();
+  } else {
+      $data = $geral["Quartos"][$_GET['index']];
+  }
+} else if ($_GET['type'] == 'mansoes') {
+  if (count($geral["Mansões"]) < intval($_GET['index'])) {
+    header('Location: Principal.php');
+      exit();
+  } else {
+      $data = $geral["Mansões"][$_GET['index']];
+  }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Área cheap</title>
+  <link rel="stylesheet"  href="style.css">
+</head>
+
+<body>
+  <header>
+
+      <a href="principal.php"> <img src="ImgCategorias/logo.png" alt="Logo Área Cheap" id="logo"> </a>
+            <form>
+              <input type="search" name="busca" placeholder="Busca">
+            </form>
+          <a href="cadastro.php">Cadastro</a>
+          <a href="login.php">Login</a>
+          <a href="saibamais.php">Saiba mais</a>
+      
+  </header>
+
+  <nav>
+    <ul>
+      <li><a href="./Principal.php">Em Alta</a></li>
+      <li><a href="./Principal.php?type=mansoes">Mansão</a></li>
+      <li><a href="./Principal.php?type=quartos">Quarto</a></li>
+      <li><a href="./Principal.php?type=fazendas">Fazenda</a></li>
+    </ul>
+  </nav>
+
+  <main>
+    <div class="listing-card">
+      <img src="<?=$data["Foto"]?>" alt="<?=$data["Localização"]?>"> 
+      <a href="principalDetalhes.php">
+      <div>
+        <h4><?=$data["Localização"]?></h4> 
+        <h2>R$<?=$data["ValorAluguel"]?> Aluguel</h2>
+        <h2>R$<?=$data["ValorVenda"]?></h2><p>Aluguel</p> 
+        <h3><?=$data["Responsável"]?></h3>  
+        <h3><?=$data["Disposição"]?></h3>  
+      </div>
+      <button>Reservar</button>
+    </a>
+		</div>  
+
+ </main>
+  <footer>
+    <div>
+    
+   <p id="pfooter"> Contatos:<br>
+    De segunda a sexta:  (77) 98812-3456<br>
+    Sabados e Domingos:  (77) 98854-6783<br>
+    Endereço:<br> IFBA Campus Brumado <br>
+   </p>
+    </div>
+    &copy; <?php echo date("Y"); ?> Meu Site Airbnb. Todos os direitos reservados.<br>
+  </footer>
+</body>
+</html>
